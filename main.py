2c48782cff7511497733250e80a06ee2f79a9661
seanrogan <srogan88@gmail.com>
2022-12-19 14:32:00 -0500

    initial commit - requirements, private values, settings values created, datadump script started

---
+++ main.py
@@ -0,0 +1,10 @@
+import scraper
+import data_dump
+
+
+def main():
+    print("main method executed")
+
+
+if __name__ == '__main__':
+    main()
---
+++ settings.py
@@ -0,0 +1,7 @@
+import private
+
+TRACK_TERMS = ["SPY", "SP500", "DOW", "GLD"]
+CSV_NAME = "tweets.csv"
+TABLE_NAME = "financial_assets"
+CONNECTION_STRING = private.CONNECTION_STRING
+
