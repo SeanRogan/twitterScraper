2806ad72ba110bf385baed69cbea1055d124a580
seanrogan <srogan88@gmail.com>
2022-12-20 20:04:20 -0500

    remade scraper to use twitter api v2, scraper connects and streams tweets correctly when tested

--- data_dump.py
+++ data_dump.py
@@ -1,8 +1,8 @@
 from datafreeze import freeze
 import dataset
 import settings
-
-
+# connects to database and dumps table to a csv file
+# todo create a way to dump these files to an s3 bucket?
 db = dataset.connect(settings.CONNECTION_STRING)
 
 result = db[settings.TABLE_NAME].all()
--- scraper.py
+++ scraper.py
@@ -26,49 +26,45 @@ class TweetStream(tweepy.StreamingClient):
         print("Connected")
 
     def on_tweet(self, tweet):
-        print(tweet)
+        # if the tweet is a retweet, do not save it
+        if tweet.retweeted:
+            return
+        # else move on to saving the tweet to the database
+        # save the needed info to variables
+        user_description = tweet.user.description
+        user_location = tweet.user.location
+        text = tweet.text
+        username = tweet.user.screen_name
+        user_created = tweet.user.created_at
+        followers = tweet.user.followers_count
+        id_str = tweet.id_str
+        tweet_created = tweet.created_at
+        retweets = tweet.retweet_count
+        blob = TextBlob(text)
+        sentiment = blob.sentiment
 
-    #
-    # def on_tweet(self, tweet):
-    #     # if the tweet is a retweet, do not save it
-    #     if tweet.retweeted:
-    #         return
-    #     # else move on to saving the tweet to the database
-    #     # save the needed info to variables
-    #     user_description = tweet.user.description
-    #     user_location = tweet.user.location
-    #     text = tweet.text
-    #     username = tweet.user.screen_name
-    #     user_created = tweet.user.created_at
-    #     followers = tweet.user.followers_count
-    #     id_str = tweet.id_str
-    #     tweet_created = tweet.created_at
-    #     retweets = tweet.retweet_count
-    #     blob = TextBlob(text)
-    #     sentiment = blob.sentiment
-    #
-    #     table = db[settings.TABLE_NAME]
-    #
-    #     try:
-    #         table.insert(dict(
-    #             user_description=user_description,
-    #             user_location=user_location,
-    #             text=text,
-    #             username=username,
-    #             tweet_created_date=tweet_created,
-    #             followers=followers,
-    #             id_str=id_str,
-    #             user_created_date=user_created,
-    #             retweets=retweets,
-    #             blob=blob,
-    #             sentiment=sentiment,
-    #         ))
-    #     except ProgrammingError as err1:
-    #         # todo need to add logging here
-    #         print("There was a ProgrammingError thrown, probably a problem with the database" + err1)
-    #     except tweepy.TweepyException as err2:
-    #         print(
-    #             "there was a TweepyException thrown, probably a problem with twitter or the rate limit has been hit" + err2)
+        table = db[settings.TABLE_NAME]
+
+        try:
+            table.insert(dict(
+                user_description=user_description,
+                user_location=user_location,
+                text=text,
+                username=username,
+                tweet_created_date=tweet_created,
+                followers=followers,
+                id_str=id_str,
+                user_created_date=user_created,
+                retweets=retweets,
+                blob=blob,
+                sentiment=sentiment,
+            ))
+        except ProgrammingError as err1:
+            # todo need to add logging here
+            print("There was a ProgrammingError thrown, probably a problem with the database" + err1)
+        except tweepy.TweepyException as err2:
+            print(
+                "there was a TweepyException thrown, probably a problem with twitter or the rate limit has been hit" + err2)
 
     def on_errors(self, err):
         print("an error occured with the tweet stream " + err)
@@ -76,10 +72,6 @@ class TweetStream(tweepy.StreamingClient):
 
 # Create the stream object
 stream = TweetStream(bearer_token=private.BEARER_TOKEN)
-current_rules = stream.get_rules()
-# reset current rules
-for rule in current_rules:
-    stream.delete_rules()
 # add new 'rules' to filter result
 for term in settings.SEARCH_TERMS:
     stream.add_rules(tweepy.StreamRule(term))
