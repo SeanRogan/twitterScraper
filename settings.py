7ed803a143682ae5397fcdc1e00bfdcaeadca533
seanrogan <srogan88@gmail.com>
2022-12-21 18:48:09 -0500

    commented out test db and uncommented prod db in setting file

--- settings.py
+++ settings.py
@@ -3,6 +3,7 @@ import private
 STREAM_FILTER_RULES = ["Meta Materials", "$MMAT"]
 CSV_NAME = "tweets.csv"
 TABLE_NAME = "tweet_data"
-# CONNECTION_STRING = private.CONNECTION_STRING
-CONNECTION_STRING = "postgresql://user:postpass@localhost:5432/postgres"
+CONNECTION_STRING = private.CONNECTION_STRING
+# testing db
+# CONNECTION_STRING = "postgresql://user:pass@localhost:5432/postgres"
 
