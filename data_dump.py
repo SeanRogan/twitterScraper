6d358bfa63c90a8b5aec39ac34e4de451280dcbc
seanrogan <srogan88@gmail.com>
2022-12-20 21:07:21 -0500

    rewrote data dump to work with command line arguments rather than a setting file.

--- data_dump.py
+++ data_dump.py
@@ -1,9 +1,23 @@
 from datafreeze import freeze
 import dataset
 import settings
-# connects to database and dumps table to a csv file
-# todo create a way to dump these files to an s3 bucket?
-db = dataset.connect(settings.CONNECTION_STRING)
+from sqlalchemy.exc import ProgrammingError
+from sys import argv
 
-result = db[settings.TABLE_NAME].all()
-freeze(result, format='csv', filename=settings.CSV_NAME)
+# This script connects to the database and dumps the table passed via argument to a csv file
+# it requires a table name and valid db connection string to be passed to it upon running the script
+# its meant to be run on a chron job by a shell script on whatever server is running the data scraper.
+# a command to run this script would look like the line below
+
+# python data_dump.py -postgresql://seanrogan:password@scraperstorage.cf2k04dkofim.us-east-1.rds.amazonaws.com:5432/scrappeddata -financial_assets -stock-related-tweets.csv
+
+
+try:
+    db = dataset.connect(argv[1])                   # connect to the database with the connection string passed via the first argument
+    result = db[argv[2]].all()                      # pull all info from the table passed by the second argument
+    freeze(result, format='csv',                    # freeze saves the info from the table into a CSV file, the name of the file
+                                                    # is passed as the third argument
+           filename=argv[3])
+except ProgrammingError as err:
+    print("an error occured with the database" + err)
+
